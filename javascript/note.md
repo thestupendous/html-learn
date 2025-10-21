7fc1b3e28089b61784b3f83d565bb746f019324b
Anand Kumar <stupendousji@gmail.com>
2025-10-21 14:40:02 +0530

    little javascript

---
+++ javascript/note.md
@@ -0,0 +1,21 @@
+## notes
+
+## variables
+word vs keyword \
+userName - variable name\
+var - keyword
+
+- scope
+  - var has function scope
+  - var ignores block scope
+  - let works with block scope too
+- temporal dead zone
+  - basically, using a 'let' defined variable before initialization
+
+- hoisting
+  - splitting of decl and def of variables
+
+## datatypes
+
+
+
---
+++ javascript/variables.js
@@ -0,0 +1,19 @@
+// var let const
+player = "super"; // bad
+var name;
+var dayNumber = 12;
+// window me add hota hai
+// function scoped hota hai
+// firse declare kar sakte hain, no error
+var dayNumber = 15;
+
+let num = 10;
+let num2 
+const num3 = 120;
+const good = prompt("you are good")
+if (good == "yes") {
+	document.getElementById("good").textContent="You are good!"
+} else {
+	document.getElementById("good").textContent="no good"
+}
+
